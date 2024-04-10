cc6591c9a3272c48985d19c49f6bd355598ee880
Shreyhan <44970171+Shreyhan@users.noreply.github.com>
2024-04-10 17:10:12 -0400

    final?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TaskModal from './TaskModal';
+import { ToastContainer, toast } from 'react-toastify';
+import { Navbar, Container, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle , faBars, faPenToSquare, faCircleXmark} from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState(null);
+
+  const handleAddTask = (task) => {
+    setTasks([...tasks, { ...task, id: Date.now(), isComplete: false }]);
+    setShowModal(false);
+  };
+
+  const handleUpdateTask = (task) => {
+    setTasks(tasks.map(t => t.id === task.id ? {...t, ...task} : t));
+    setCurrentTask(null);
+    setShowModal(false);
+  }
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, isComplete: !task.isComplete } : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+    toast.success("Task deleted successfully!");
+  };
+
+  const openTaskModal = (task) => {
+    setCurrentTask(task);
+    setShowModal(true);
+  };
+
+  const editedDate = (dateVal) => {
+    const dateParts = dateVal.split('-');
+    const yearTwoDigits = dateParts[0].slice(-2);
+    return `${dateParts[1]}/${dateParts[2]}/${yearTwoDigits}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+        <ToastContainer position="bottom-right"/>
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Container>
+                <p className="mx-auto"><FontAwesomeIcon icon={faBars}/> FRAMEWORKS</p>
+                <Button className="mx-right" variant="outline-light" onClick={() => openTaskModal(null)}><FontAwesomeIcon icon={faPlusCircle}/> ADD</Button>
+            </Container>
+        </Navbar>
+
+        <table className="table table-bordered mt-4">
+            <thead className="thead-dark">
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Deadline</th>
+                <th>Priority</th>
+                <th>Is Complete</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task) => (
+                    <tr key={task.id}>
+                    <td>{task.title}</td>
+                    <td>{task.description}</td>
+                    <td>{editedDate(task.deadline)}</td>
+                    <td>{task.priority}</td>
+                    <td>
+                        <input
+                        type="checkbox"
+                        checked={task.isComplete}
+                        onChange={() => toggleTaskCompletion(task.id)}
+                        />
+                    </td>
+                    <td>
+                        <div className="vertical-buttons">
+                            {!task.isComplete && (
+                              <button className="btn btn-primary" onClick={() => openTaskModal(task)}><FontAwesomeIcon icon={faPenToSquare}/> Update</button>
+                            )}
+                            <button className="btn btn-danger" onClick={() => deleteTask(task.id)}><FontAwesomeIcon icon={faCircleXmark}/> Delete</button>
+                        </div>
+                    </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <TaskModal showModal={showModal} closeModal={() => setShowModal(false)} addTask={handleAddTask} updateTask={handleUpdateTask} currentTask={currentTask} tasks={tasks}/>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/TaskModal.js
@@ -0,0 +1,132 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle , faPenToSquare, faBan} from '@fortawesome/free-solid-svg-icons';
+import { toast } from 'react-toastify';
+
+const TaskModal = ({ showModal, closeModal, addTask, updateTask, currentTask, tasks}) => {
+  const [task, setTask] = useState({
+    title: '',
+    description: '',
+    deadline: '',
+    priority: 'med',
+  });
+
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (showModal) {
+      if (currentTask) {
+        setTask(currentTask);
+      } else {
+        setTask({
+          title: '',
+          description: '',
+          deadline: '',
+          priority: 'med',
+        });
+      }
+    }
+  }, [currentTask, showModal]);
+
+  const validateForm = () => {
+    let formIsValid = true;
+    let errors = {};
+
+    if (!task.title && !currentTask) {
+        formIsValid = false;
+        errors['title'] = 'Title is Required!';
+    }
+    if (!task.description) {
+        formIsValid = false;
+        errors['description'] = 'Description is Required!';
+    }
+    if (!task.deadline) {
+        formIsValid = false;
+        errors['deadline'] = 'Deadline is Required!';
+    }
+    if (!currentTask && tasks.some(t => t.title.toLowerCase() === task.title.toLowerCase())) {
+        formIsValid = false;
+        errors['title'] = 'Task with this title already exists!';
+    }
+
+    setErrors(errors);
+    return formIsValid;
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTask({ ...task, [name]: value });
+    setErrors({ ...errors, [name]: '' });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      if (currentTask) {
+        updateTask({...task, id: currentTask.id});
+      } else {
+        addTask(task);
+      }
+      closeModal();
+      toast.success(`Task ${currentTask ? 'updated' : 'added'} successfully!`);
+    }
+  };
+
+  return (
+    <div className={`modal fade ${showModal ? 'show' : ''}`} style={{ display: showModal ? 'block' : 'none' }} tabIndex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header bg-primary">
+            <h5 className="modal-title">
+                {currentTask ? <><FontAwesomeIcon icon={faPenToSquare}/> Edit Task</> : <><FontAwesomeIcon icon={faPlusCircle}/> Add Task</>}
+            </h5>
+          </div>
+          <div className="modal-body panel-body">
+            <form>
+                {!currentTask && (
+                    <div className="form-group">
+                        <label>Title</label>
+                        <input type="text" className={`form-control ${errors.title ? 'is-invalid' : ''}`} name="title" id="title" placeholder="Title" value={task.title} required onChange={handleChange}/>
+                        {errors.title && <div className="invalid-feedback">{errors.title}</div>}
+                    </div>
+                )}
+              <div className="form-group">
+                <label>Description</label>
+                <textarea className={`form-control ${errors.description ? 'is-invalid' : ''}`} name="description" placeholder="Description" value={task.description} required onChange={handleChange}></textarea>
+                {errors.description && <div className="invalid-feedback">{errors.description}</div>}
+              </div>
+              <div className="form-group">
+                <label>Deadline</label>
+                <input type="date" className={`form-control ${errors.deadline ? 'is-invalid' : ''}`} name="deadline" value={task.deadline} required onChange={handleChange}/>
+                {errors.deadline && <div className="invalid-feedback">{errors.deadline}</div>}
+              </div>
+              <div className="form-group">
+                <label>Priority</label>
+                <div id="radioButtons">
+                    <div className="form-check addspace">
+                        <input type="radio" className="form-check-input" name="priority" id="priorityLow" value="low" checked={task.priority === 'low'} required onChange={handleChange}/>
+                        <label className="form-check-label">Low</label>
+                    </div>
+                    <div className="form-check addspace">
+                        <input type="radio" className="form-check-input" name="priority" id="priorityMed" value="med" checked={task.priority === 'med'} required onChange={handleChange}/>
+                        <label className="form-check-label">Med</label>
+                    </div>
+                    <div className="form-check">
+                        <input type="radio" className="form-check-input" name="priority" id="priorityHigh" value="high" checked={task.priority === 'high'} required onChange={handleChange}/>
+                        <label className="form-check-label">High</label>
+                    </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-primary" onClick={handleSubmit}>{currentTask ? <><FontAwesomeIcon icon={faPenToSquare}/> EDIT</> : <><FontAwesomeIcon icon={faPlusCircle}/> ADD</>}</button>
+            <button type="button" className="btn btn-danger" onClick={closeModal}><FontAwesomeIcon icon={faBan}/> Cancel</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskModal;
